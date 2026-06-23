825e61356016ae7dda1a758134d26148f35205df
quophiBonney <quophiclef@gmail.com>
2026-06-23 16:39:55 +0000

    added map

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Footer from "./components/Footer";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Menus from "./pages/Menus";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menus" element={<Menus />} />
+        <Route path="/contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Map from "../components/Map";
+
+const Contact = () => {
+  return (
+    <>
+      <Map />
+    </>
+  );
+};
+
+export default Contact;
