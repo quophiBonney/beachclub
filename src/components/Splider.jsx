8946e940b8ff6a39bc4632a988e759411398cac5
quophiBonney <quophiclef@gmail.com>
2026-06-24 16:03:54 +0000

    fixing hero height on tablet

--- src/components/Splider.jsx
+++ src/components/Splider.jsx
@@ -64,7 +64,7 @@ const Splider = () => {
   const [activeIndex, setActiveIndex] = useState(0);
 
   return (
-    <section className="relative w-full overflow-hidden bg-black h-[60vh] lg:h-screen">
+    <section className="relative w-full overflow-hidden bg-black h-[60vh] md:h-[65vh] lg:h-screen">
       <Swiper
         modules={[Navigation, Pagination, A11y, Autoplay, EffectFade]}
         slidesPerView={1}
