c2079b19907d454e63cf46dafb86801b200e634d
quophiBonney <quophiclef@gmail.com>
2026-06-24 14:00:14 +0000

    fix the word

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import AboutHero from "../components/AboutHero";
+import ServicesCard from "../components/ServicesCard";
 
 const AboutUs = () => {
-  return <AboutHero />;
+  return (
+    <>
+      <AboutHero />
+      <ServicesCard />
+    </>
+  );
 };
 
 export default AboutUs;
