8f8b0d2d5874aa7ebc799afdbeb587850e0bbaba
quophiBonney <quophiclef@gmail.com>
2026-06-24 14:25:33 +0000

    about page ongoing

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import AboutHero from "../components/AboutHero";
 import ServicesCard from "../components/ServicesCard";
+import CEOMessage from "../components/CEOMessage";
+import ManagementProfiles from "../components/ManagementProfiles";
 
 const AboutUs = () => {
   return (
     <>
       <AboutHero />
       <ServicesCard />
+      <CEOMessage />
+      <ManagementProfiles />
     </>
   );
 };
